1ff12189021e4d48a2771aa36d76b5a8d1f8d993
Ife-Olaitan <olaiyaifeoluwa.o@gmail.com>
2026-01-24 11:45:32 +0000

    feat: update networking and storage module output

--- terraform/modules/storage/outputs.tf
+++ terraform/modules/storage/outputs.tf
@@ -1,19 +1,19 @@
 output "bucket_name" {
   description = "Name of the S3 bucket"
-  value       = aws_s3_bucket.bucket.id
+  value       = aws_s3_bucket.starttech_bucket.id
 }
 
 output "bucket_arn" {
   description = "ARN of the S3 bucket"
-  value       = aws_s3_bucket.bucket.arn
+  value       = aws_s3_bucket.starttech_bucket.arn
 }
 
 output "cloudfront_distribution_id" {
   description = "ID of the CloudFront distribution (for cache invalidation)"
-  value       = aws_cloudfront_distribution.distribution.id
+  value       = aws_cloudfront_distribution.starttech_distribution.id
 }
 
 output "cloudfront_domain_name" {
   description = "Domain name of the CloudFront distribution"
-  value       = aws_cloudfront_distribution.distribution.domain_name
+  value       = aws_cloudfront_distribution.starttech_distribution.domain_name
 }
